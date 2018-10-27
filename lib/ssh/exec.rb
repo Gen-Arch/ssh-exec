a500e45d79af6f2c572673ae1b624799769cce8c
Gen <ymdtshm@gmail.com>
2018-10-27 11:39:17 +0900

    add session

--- lib/ssh/exec.rb
+++ lib/ssh/exec.rb
@@ -1,6 +1,5 @@
 $: << __dir__
 
-
 module SSH
   module Exec
   require "exec/session"
@@ -8,7 +7,7 @@ module SSH
 end
 
 if __FILE__ == $0
-  session = SSH::Exec::Session.new
-  out, err = session.exec("ls -al")
-  puts out
+  session = SSH::Exec::Session.new("192.168.0.104", "gen", :keys => "#{ENV['HOME']}/.ssh/gen-server")
+  res = session.stdin(["ls -al", "hostname", "echo aaaa"], :shell => "bash -l")
+  pp res
 end
--- lib/ssh/exec/session.rb
+++ lib/ssh/exec/session.rb
@@ -2,40 +2,56 @@ require 'net/ssh'
 
 module SSH::Exec
   class Session
+    attr_accessor :host
+    attr_accessor :id
+    attr_accessor :shell
+    attr_accessor :result
+    attr_accessor :streams
     attr_accessor :session
 
-    def initialize
-      @host = '192.168.0.104'
-      @name = 'gen'
-      @option = {:keys => ["#{ENV['HOME']}/.ssh/gen-server"]}
-      @session = Net::SSH.start(@host, @name, @option)
+    def initialize(host, id, **opt)
+      @host = host
+      @id = id
+      @option = opt
+      @result = Array.new
+      @session = Net::SSH.start(@host, @id, @option)
     end
 
-    def exec(command)
-      stdout = String.new
-      stderr = String.new
-      channel = @session.open_channel do |ch|
-        ch.exec(command) do |ch, success|
-          raise "sesstion error!!" unless success
-          ch.on_data do |c,data|
-            stdout =  data
-          end
+    def stdin(cmd, **opt)
+      @shell = (opt[:shell] || nil)
+      @streams = {:stdin => cmd}
+      open
 
-          ch.on_extended_data do |c,type,data|
-            stderr data
-          end
-        end
-        ch.on_close
-      end
+      result << streams
+      return streams
+    end
+
+    def open
+      channel = session.open_channel(&method(:open_succeeded))
       channel.wait
-      return stdout, stderr
+    end
+
+    def open_succeeded(channel)
+      channel.exec(shell?, &method(:cmd_exec))
+      channel.on_close
+    end
+
+    def cmd_exec(channel, success)
+      raise "sesstion error!!" unless success
+      unless shell.nil?
+        streams[:stdin].each{|cmd| channel.send_data "#{cmd}\n"} if streams[:stdin].is_a?(Array)
+        channel.send_data "#{streams[:stdin]}\n" if streams[:stdin].is_a?(String)
+        channel.send_data "exit\n"
+      end
+      channel.on_data{|c,data| streams.merge!(:stdout => data)}
+      channel.on_extended_data{|c,type,data| streams.merge!(:stderr => data)}
+    end
+
+    def shell?
+      return streams[:stdin] if shell.nil?
+      return shell
     end
 
   end
 end
 
-if __FILE__ == $0
-  session = SSH::Exec::Session.new
-  out, err = session.exec("ls -al")
-  puts out
-end
